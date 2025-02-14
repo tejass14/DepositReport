f4ea593f08da17654bc590ee74481b1a48ae3947
tejass <tejassawant@benchmarkit.solutions>
2025-02-14 11:09:53 +0530

    feat : added entities

--- DepositReport.Domain/Entities/Merchants.cs
+++ DepositReport.Domain/Entities/Merchants.cs
@@ -1,21 +1,23 @@
-﻿namespace DepositReport.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DepositReport.Domain.Entities
 {
     public class Merchants
     {
-        public int MerchantID { get; set; }
+        public int MerchantId { get; set; }
         public string AccountName { get; set; }
-        public int? OrganizationID { get; set; }
-        public string OrganizationName { get; set; }
-        public string MerchantCode { get; set; }
-        public string MID { get; set; }
-        public string ProcessorMID { get; set; }
-        public string SettlementAccount { get; set; }
-        public string SettlementRoutingNo { get; set; }
-        public string SourceSystemIdentifier { get; set; }
+        public int? OrganizationId { get; set; }
+        public string? OrganizationName { get; set; }
+        public string? MerchantCode { get; set; }
+        public string? MID { get; set; }
+        public string? ProcessorMID { get; set; }
+        public string? SettlementAccount { get; set; }
+        public string? SettlementRoutingNo { get; set; }
+        public string? SourceSystemIdentifier { get; set; }
         public int? MCID { get; set; }
-        public string MIDType { get; set; }
-        public string Category { get; set; }
-        public string DataSource { get; set; }
+        public string? MIDType { get; set; }
+        public string? Category { get; set; }
+        public string? DataSource { get; set; }
         public DateTime? LastDepositDate { get; set; }
         public int? Reportable { get; set; }
     }
---
+++ DepositReport.Domain/Entities/ReportTransactionsTemp.cs
@@ -0,0 +1,56 @@
+﻿namespace DepositReport.Domain.Entities
+{
+    public class ReportTransactionsTemp
+    {
+        public string? RowUid { get; set; }
+        public string? HasMatchedData { get; set; }
+        public string? TsysMerchantAccountNumber { get; set; }
+        public string? TsysPurchaseId { get; set; }
+        public string? TsysTransactionDate { get; set; }
+        public string? TsysAuthAmount { get; set; }
+        public string? TsysTransactionAmount { get; set; }
+        public string? TsysAuthorizationNumber { get; set; }
+        public string? TsysCardholderAccountNumber { get; set; }
+        public string? TransactionId { get; set; }
+        public string? PvGatewayAuditId { get; set; }
+        public string? TransactionReferenceCode { get; set; }
+        public string? TransactionStatusTypeName { get; set; }
+        public string? OriginalSubmittedTransactionRefCode { get; set; }
+        public string? TransactionCancelledTypeName { get; set; }
+        public string? CaptureTrackingNumberId { get; set; }
+        public string? CaptureTrackingNumber { get; set; }
+        public string? UserDefinedFieldOne { get; set; }
+        public string? UserDefinedFieldTwo { get; set; }
+        public string? ConfirmationNumber { get; set; }
+        public string? CaptureSourceTypeName { get; set; }
+        public string? LoginName { get; set; }
+        public string? UserFullName { get; set; }
+        public string? OrganizationId { get; set; }
+        public string? PayeeCode { get; set; }
+        public string? PayeeName { get; set; }
+        public string? MerchantCode { get; set; }
+        public string? MerchantName { get; set; }
+        public string? AuthorizationCode { get; set; }
+        public string? AccountName { get; set; }
+        public string? CustomerFullName { get; set; }
+        public string? CustomerReferenceCode { get; set; }
+        public string? AccountReferenceCode { get; set; }
+        public string? CardNumberMasked { get; set; }
+        public string? CardNumberWithType { get; set; }
+        public string? CardExpDate { get; set; }
+        public string? SettlementDate { get; set; }
+        public string? Amount { get; set; }
+        public string? ValidationBatchId { get; set; }
+        public string? DebitCreditTypeName { get; set; }
+        public string? TransactionCancelledTypeId { get; set; }
+        public string? OriginalSubmittedTransactionId { get; set; }
+        public string? PvGatewayRequestTypeId { get; set; }
+        public string? ValidationBatchName { get; set; }
+        public string? UserDefinedBatchName { get; set; }
+        public string? CreatedDate { get; set; }
+        public string? LastUpdatedDate { get; set; }
+        public int? DepositId { get; set; }
+        public string? SessionId { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}

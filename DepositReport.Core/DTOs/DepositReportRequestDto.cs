8d0c682bb34e80ff72484fcda6b52b26b5848731
tejass <tejassawant@benchmarkit.solutions>
2025-02-14 12:56:51 +0530

    refactor : code refactor

--- DepositReport.Core/DTOs/DepositReportRequestDto.cs
+++ DepositReport.Core/DTOs/DepositReportRequestDto.cs
@@ -1,22 +1,27 @@
-﻿namespace DepositReport.Core.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DepositReport.Core.DTOs
 {
     public class DepositReportRequestDto
     {
         public string OrganizationName { get; set; }
-        public int MCID { get; set; }
-        public int OrganizationID { get; set; }
-        public int DepositReportID { get; set; }
-        public int CCMIDs { get; set; }
+        [Display(Name = "MCID")]
+        public int Mcid { get; set; }
+        public int OrganizationId { get; set; }
+        public int DepositReportId { get; set; }
+        [Display(Name = "CCMIDS")]
+        public int CcmiDs { get; set; }
         public string ReportXml { get; set; }
         public DateTime? ReportDate { get; set; }
-        public DateTime ?TSYSDate { get; set; }
-        public string ACHMIDs { get; set; }
+        public DateTime? TsysDate { get; set; }
+        public string AchmiDs { get; set; }
         public string XmlName { get; set; }
-        public string CCSummary { get; set; }
-        public string ACHSummary { get; set; }
-        public string c_DepositReport { get; set; }
+        public string CcSummary { get; set; }
+        public string AchSummary { get; set; }
+        [Display(Name = "c_DepositReport")]
+        public string CDepositReport { get; set; }
         // public string iFile { get; set; } 
-        public string Reportxml { get; set; } 
-        public int xlsxReport { get; set; }
+        public string Reportxml { get; set; }
+        public int XlsxReport { get; set; }
     }
 }
